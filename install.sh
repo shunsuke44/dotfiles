4043fed02313c6f5b84fac21d0a8af0299a1a081
Shunsuke Shimura <shunsuke.shimura@satori.team>
2023-04-18 09:35:35 +0000

    fix

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ set -eu -o pipefail
 # except this script
 for fname in $(git ls-files | grep -v install.sh); do
     absname="$(readlink -f $fname)"
-    echo $absname
     if [[ -f "$HOME/$fname" ]]; then
         mv "$HOME/$fname" "$HOME/${fname}${origext}"
     fi
