eba66296ba41030cfdadaef9b4efb88301df7de3
Shunsuke Shimura <shunsuke.shimura@satori.team>
2023-04-19 09:02:29 +0000

    Move .tmux.conf to global

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ case "$(uname -s)" in
        exit 1
 esac
 
-files=('.bash_profile' '.bashrc' '.git-prompt.sh')
+files=('.bash_profile' '.bashrc' '.git-prompt.sh' '.tmux.conf')
 
 # install global dotfiles
 echo 'installing global dotfiles...'
@@ -25,25 +25,27 @@ for fname in ${files[@]}; do
     echo "installed $fname"
 done
 
-# install local dotfiles
-echo 'installing local dotfiles...'
-localfiles=('.tmux.conf')
-localdir='local'
-case "$machine" in
-    'linux') localdir="$localdir/linux";;
-    'mac') localdir="$localdir/mac";;
-    'unknown') localdir=''
-esac
-
-if [[ "$localdir" ]]; then
-    echo "installing local dotfiles in $localdir ..."
-    for fname in ${localfiles[@]}; do
-        absname="$(readlink -f "$localdir/$fname")"
-        if [[ -f "$HOME/$fname" ]]; then
-            mv "$HOME/$fname" "$HOME/${fname}${origext}"
-        fi
-        ln "$absname" "$HOME/$fname"
-    done
-fi
-
 exit 0
+
+# # install local dotfiles
+# echo 'installing local dotfiles...'
+# localfiles=('.tmux.conf')
+# localdir='local'
+# case "$machine" in
+#     'linux') localdir="$localdir/linux";;
+#     'mac') localdir="$localdir/mac";;
+#     'unknown') localdir=''
+# esac
+#
+# if [[ "$localdir" ]]; then
+#     echo "installing local dotfiles in $localdir ..."
+#     for fname in ${localfiles[@]}; do
+#         absname="$(readlink -f "$localdir/$fname")"
+#         if [[ -f "$HOME/$fname" ]]; then
+#             mv "$HOME/$fname" "$HOME/${fname}${origext}"
+#         fi
+#         ln "$absname" "$HOME/$fname"
+#     done
+# fi
+#
+# exit 0
